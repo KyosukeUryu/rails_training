2eb819d4623e1a3a32af3e9cf4466c4d78e19c45
kyosuke uryu <kyo.uryu33@gmail.com>
2019-11-01 17:50:51 +0900

    model controller view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,20 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(choose_params)
+    if @contact.save
+      redirect_to new_contact_path , notice: 'お問い合わせを投稿しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def choose_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
 end
